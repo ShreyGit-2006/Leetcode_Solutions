e6ed9925f85e5a1f7f5722f49fe386cb27fec443
ShreyGit-2006 <shreyanshpudipeddi@gmail.com>
2026-07-29 13:46:06 +0530

    Time: 0 ms (100%), Space: 46.1 MB (43.06%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,6 +1,6 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double kelvin;
+         double kelvin;
         double fahrenheit;
         kelvin=celsius+273.15;
         fahrenheit=(celsius*1.80)+32.00;
