eba8230097ab9e7858ef773c5537d4fcf90f8259
ShreyGit-2006 <shreyanshpudipeddi@gmail.com>
2026-08-01 16:52:57 +0530

    Time: 39 ms (15.45%), Space: 43.1 MB (80.9%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.java
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.java
@@ -4,53 +4,42 @@ class Solution {
         int count1 = 0;
         int sum = 0;
         int sum1 = 0;
-
         for (int i = low; i <= high; i++) {
-
             count = 0;
             sum = 0;
             sum1 = 0;
-
             int temp = i;
             int temp1 = i;
             int temp2 = i;
             int rev = 0;
-
             while (temp2 > 0) {
                 int rem = temp2 % 10;
                 rev = rev * 10 + rem;
                 temp2 /= 10;
             }
-
             while (temp > 0) {
                 temp /= 10;
                 count++;
             }
-
             if (count % 2 == 0) {
-
                 int t = count / 2;
                 int t1 = t;
-
                 while (t > 0) {
                     int rem = temp1 % 10;
                     sum += rem;
                     temp1 /= 10;
                     t--;
                 }
-
                 while (t1 > 0) {
                     int rem = rev % 10;
                     sum1 += rem;
                     rev /= 10;
                     t1--;
                 }
-
                 if (sum == sum1)
                     count1++;
             }
         }
-
         return count1;
     }
 }
