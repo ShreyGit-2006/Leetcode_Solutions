d9a303e9f4757d32d1ac979d15ae44776b823fbc
ShreyGit-2006 <shreyanshpudipeddi@gmail.com>
2026-08-02 10:46:05 +0530

    Time: 0 ms (100%), Space: 10.2 MB (45.81%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.c
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.c
@@ -1,6 +1,5 @@
 bool isToeplitzMatrix(int** matrix, int matrixSize, int* matrixColSize) {
     int col=matrixColSize[0];
-    int sum=0;
     for(int i=1;i<matrixSize;i++)
     {
         for(int j=1;j<col;j++)
